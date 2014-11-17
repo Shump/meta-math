fe20b61bf6c73443f0715c050c96f4cd9ca6d0ff
Julian Lindblad <JulianLindblad@gmail.com>
2014-11-17 19:29:47 +0900

    Reformated the tests, added power test.

--- main.cpp
+++ main.cpp
@@ -3,43 +3,64 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
 
-template<long long N1, long long D1,
-         long long N2, long long D2,
-         template<typename A, typename B> class Op>
+template<typename Frac>
+std::string frac2str() {
+  std::stringstream ss;
+  ss << Frac::Nom << " / " << Frac::Denom << " (" << Frac::value() << ")";
+  return ss.str();
+};
+
+template<typename frac1,
+         typename frac2,
+         template<typename, typename> class Op>
 struct BinaryTester {
-  using frac1 = Fraction<N1, D1>;
-  using frac2 = Fraction<N2, D2>;
   using result = typename Op<frac1, frac2>::value;
 
   static void run(std::string test_name) {
     std::cout << "Running: " << test_name << std::endl;
     std::cout << result::Nom << " / " << result::Denom << " = " << result::value() << std::endl;
   };
-
 };
 
 void test_add() {
-  BinaryTester<1,3,1,3,FractionAdd>::run("Addition");
+  using a = Fraction<1,3>;
+  using b = Fraction<1,3>;
+  BinaryTester<a,b,FractionAdd>::run("Addition");
 };
 
 void test_sub() {
-  BinaryTester<2,3,1,3,FractionSub>::run("Subtraction");
+  using a = Fraction<2,3>;
+  using b = Fraction<1,3>;
+  BinaryTester<a,b,FractionSub>::run("Subtraction");
 };
 
 void test_div() {
-  BinaryTester<1,3,3,1,FractionDiv>::run("Division");
+  using a = Fraction<1,3>;
+  using b = Fraction<3,1>;
+  BinaryTester<a,b,FractionDiv>::run("Division");
 }
 
 void test_mul() {
-  BinaryTester<1,3,1,3,FractionMul>::run("Multiplication");
+  using a = Fraction<1,3>;
+  using b = Fraction<1,3>;
+  BinaryTester<a,b,FractionMul>::run("Multiplication");
 };
+
+void test_pow() {
+  using a = Fraction<2,1>;
+  using result = FractionPow<a, 3>::value;
+  std::cout << frac2str<result>() << std::endl;
+}
 
 int main() {
   test_add();
   test_sub();
   test_mul();
   test_div();
+
+  test_pow();
 }
 
 
